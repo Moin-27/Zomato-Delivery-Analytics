5307066ab381c33423ba8c048f14b4b343c9d820
Moin Ahmed <moinahmed9869@gmail.com>
2026-05-26 21:46:38 +0530

    Update traffic_analysis.py

--- Python Files/traffic_analysis.py
+++ Python Files/traffic_analysis.py
@@ -1,20 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Dark Theme
+plt.style.use("dark_background")
+
 # Load Dataset
 df = pd.read_csv("Dataset/Zomato Dataset.csv")
 
 # Average Delivery Time by Traffic Level
-traffic_data = df.groupby("Road_traffic_density")["Time_taken (min)"].mean()
+traffic_data = df.groupby(
+    "Road_traffic_density"
+)["Time_taken (min)"].mean()
 
 # Create Figure
-plt.figure(figsize=(8,5))
+fig = plt.figure(figsize=(8,5))
 
-# Plot Chart
-traffic_data.plot(kind='bar')
+# Background Color
+fig.patch.set_facecolor("#0f172a")
+
+# Plot Bar Chart
+traffic_data.plot(
+    kind='bar',
+    color=["salmon","khaki","mediumseagreen","deepskyblue"]
+)
 
 # Title and Labels
-plt.title("Average Delivery Time by Traffic Level", fontsize=14)
+plt.title(
+    "Average Delivery Time by Traffic Level",
+    fontsize=14,
+    fontweight="bold"
+)
+
 plt.xlabel("Traffic Level", fontsize=12)
 plt.ylabel("Average Delivery Time (mins)", fontsize=12)
 
@@ -22,7 +38,14 @@ plt.ylabel("Average Delivery Time (mins)", fontsize=12)
 plt.xticks(rotation=15)
 
 # Add Grid
-plt.grid(axis='y', linestyle='--', alpha=0.5)
+plt.grid(
+    axis='y',
+    linestyle='--',
+    alpha=0.1
+)
+
+# Graph Background
+plt.gca().set_facecolor("#111827")
 
 # Show Graph
 plt.show()
