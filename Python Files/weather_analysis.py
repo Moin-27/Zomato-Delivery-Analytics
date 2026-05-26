ef43af58e5a95dc074b81d40ee2142a25895f3ad
Moin Ahmed <moinahmed9869@gmail.com>
2026-05-26 21:46:25 +0530

    Update weather_analysis.py

--- Python Files/weather_analysis.py
+++ Python Files/weather_analysis.py
@@ -1,20 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Dark Theme
+plt.style.use("dark_background")
+
 # Load Dataset
 df = pd.read_csv("Dataset/Zomato Dataset.csv")
 
 # Average Delivery Time by Weather
-weather_data = df.groupby("Weather_conditions")["Time_taken (min)"].mean()
+weather_data = df.groupby(
+    "Weather_conditions"
+)["Time_taken (min)"].mean()
 
 # Create Figure
-plt.figure(figsize=(10,5))
+fig = plt.figure(figsize=(10,5))
+
+# Background Color
+fig.patch.set_facecolor("#0f172a")
 
 # Plot Bar Chart
-weather_data.plot(kind='bar')
+weather_data.plot(
+    kind='bar',
+    color="mediumseagreen"
+)
 
 # Title and Labels
-plt.title("Average Delivery Time by Weather Conditions", fontsize=14)
+plt.title(
+    "Average Delivery Time by Weather Conditions",
+    fontsize=14,
+    fontweight="bold"
+)
+
 plt.xlabel("Weather Conditions", fontsize=12)
 plt.ylabel("Average Delivery Time (mins)", fontsize=12)
 
@@ -22,7 +38,14 @@ plt.ylabel("Average Delivery Time (mins)", fontsize=12)
 plt.xticks(rotation=20)
 
 # Add Grid
-plt.grid(axis='y', linestyle='--', alpha=0.5)
+plt.grid(
+    axis='y',
+    linestyle='--',
+    alpha=0.1
+)
+
+# Graph Background
+plt.gca().set_facecolor("#111827")
 
 # Show Graph
 plt.show()
